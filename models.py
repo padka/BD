03c210cdc704da68c705aa0a61e52d0933ee4d92
padka <96015625+padka@users.noreply.github.com>
2021-12-12 20:32:18 +0300

    BD
    BD

---
+++ models.py
@@ -0,0 +1,8 @@
+﻿from django.db import models
+
+class Bb(models.Model):
+    fio = models.CharField(max_length = 90)
+    number_dela = models.IntegerField()
+    education = models.CharField(max_length = 90)
+    price = models.IntegerField()
+
---
+++ views.py
@@ -0,0 +1,8 @@
+﻿from django.http import HttpResponse
+from .models import Bb
+
+def index(request):
+    s = 'Список сотрудников:\r\n\r\n\r\n'
+    for bb in Bb.objects.all():
+        s += 'Сотрудник:  ' + bb.fio + '\r\n' + 'Образование:  ' + bb.education + '\r\n\r\n';
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')

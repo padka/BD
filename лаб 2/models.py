53c1ce81f4bc61e0f02f3a5abd51aa5cb8329f12
padka <96015625+padka@users.noreply.github.com>
2021-12-13 20:19:39 +0300

    BD 2
    BD 2

---
+++ лаб 2/models.py
@@ -0,0 +1,33 @@
+﻿# База данных Начисление зарплаты
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
+
+class Dolznost(models.Model):
+    salary_per_hour = models.IntegerField(verbose_name = "Оклад в час")
+
+class Zarplata(models.Model):
+    data_viplat = models.DateField(verbose_name = "Дата выплаты")
+    hours_worked = models.IntegerField(verbose_name = "Фактически отработанные часы")
+    amount_to_issue = models.IntegerField(verbose_name = "Сумма к выдаче")
+
+class Priem_na_raboty(models.Model):
+    id_sotr = models.ForeignKey(Dolznost, on_delete=models.CASCADE, verbose_name = "Код_сотрудника")
+    data_dismissals = models.DateField(verbose_name = "Дата увольнения")
+    dolya = models.IntegerField(verbose_name = "Доля ставки")
+
+class Licnoe_delo(models.Model):
+    id_sotr = models.ForeignKey(Priem_na_raboty, on_delete=models.CASCADE, verbose_name = "Код_сотрудника")
+    fio =  models.CharField(max_length = 90, verbose_name = "ФИО сотрудника")
+    qualification = models.CharField(max_length = 50, verbose_name = "Квалификация")
+    education = models.CharField(max_length = 60, verbose_name = "Образование")
+    data_priema = models.DateField(verbose_name = "Дата приема")
+    number_dela = models.IntegerField(verbose_name = "Номер личного дела")
+
+
+
+
+
+
+
